3e74ad8186881c280cec9a80ce681ce1312c2a0e
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-07 18:26:23 +0330

    JS added

--- script.js
+++ script.js
@@ -4,5 +4,25 @@ console.log(40 + 8 + 23 - 10);
 console.log("Ali");
 console.log("31");
 
-let firstName = "Ali";
-console.log(firstName)
+let firstName = "Pooneh";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Hacker";
+
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
+let country = "IRAN";
+let continent = "Asia";
+let population = "80/000/000 M";
+
+
+console.log(country);
+console.log(continent);
+console.log(population);
