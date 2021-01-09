6c5b70ba3efe9a79ea9454dd5f093db779eb8c42
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-09 16:15:14 +0330

    conversion and coersion

--- script.js
+++ script.js
@@ -149,3 +149,31 @@ console.log('10' > '3');
 let n = '1' + 1; // "11"
 n = n - 1;
 console.log(n);
+
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Ali"));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
+
+
+const money = 100;
+if (money) {
+    console.log("don't spend it all");
+
+
+} else {
+    console.log("you sholud get a job");
+}
+
+let height;
+
+if (height) {
+    console.log('YaY! height is defined')
+
+} else {
+    console.log('height is undefined')
+}
