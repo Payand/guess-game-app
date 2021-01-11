0b453048fbaf3de33b69a03d92946ef2a0e32ee7
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-11 18:12:14 +0330

    function added

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ logger()
 
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges)
+
     const juice = `Ali has ${apples} apples and ${oranges} oranges.`;
     return juice;
 
@@ -20,3 +20,17 @@ function fruitProcessor(apples, oranges) {
 }
 fruitProcessor(5, 3);
 console.log(fruitProcessor(5, 3));
+
+
+
+const appleOrangeJucie = fruitProcessor(2, 4);
+console.log(appleOrangeJucie);
+
+
+
+
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear
+    return age;
+}
+console.log(calcAge1(1989));
