7332c7ac0da3c5db43b935052326a79e4fdb9d14
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-11 17:40:53 +0330

    function added

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+'use strict';
+
+
+
 function logger() {
     console.log("my name is Ali");
 
 }
 
 logger()
+
+
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges)
+    const juice = `Ali has ${apples} apples and ${oranges} oranges.`;
+    return juice;
+
+
+}
+fruitProcessor(5, 3);
+console.log(fruitProcessor(5, 3));
