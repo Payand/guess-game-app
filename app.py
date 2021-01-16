f251bb368abf95cfc028b074f484f5c3c2a99215
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-16 16:01:03 +0330

    new content added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 
-
+'''
 def random_number():
     comp_number = random.randrange(0, 20)
     return comp_number
@@ -24,3 +24,40 @@ def game_rational():
 
 
 print(game_rational())
+'''
+# list-methods
+list_names = ['micheal', 'steven', 'peter']
+
+print(list_names)
+print(list_names[0])
+print(len(list_names))
+list_names[2] = 'Joey'
+print(list_names)
+
+
+def age_years(birthyear):
+    return 2021 - birthyear
+
+
+years = [1988, 1989, 1990, 1991]
+
+
+def age_calc():
+    global years
+    age = []
+    for year in years:
+        if year < 1990:
+
+            age.append(age_years(year))
+
+            print(list(age))
+
+
+age_calc()
+
+# age = []
+# for year in years:
+
+#     hello = age_years(year)
+#     age.append(hello)
+#     print(list(age))
