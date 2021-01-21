9850086beab22175875a8cc1d3de52a9fc31e67c
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-21 19:04:38 +0330

    guess my number

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 'use strict';
-
+/*
 document.querySelector('.message').textContent = 'Correct number!!';
 
 console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+document.querySelector('.number').textContent = '??';
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 10;
+*/
