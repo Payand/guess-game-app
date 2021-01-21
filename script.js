3a32cb9da742def3d3c3277f588549b45807552a
Ali Shooshtari <shooshtariali05@gmail.com>
2021-01-21 21:33:28 +0330

    guess my number

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 10;
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -25,7 +26,11 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('.highscore').textContent = score;
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
 
         // when guess too high
     } else if (guess > secretNumber) {
